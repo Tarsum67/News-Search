cb54dc2e932f41f7020077b057f324179e31c6ac
Tarsum67 <taraum@icloud.com>
2025-01-03 17:43:45 -0600

    finished html and css started js

---
+++ index.js
@@ -0,0 +1,10 @@
+const apikey = "eb993dce9d2745be97d320eaa48b2f7c";
+
+const blogContainer = document.getElementById("blog-container");
+
+function fetchRandomNews() {
+  try {
+  } catch (error) {
+    console.error("Error fetching random news");
+  }
+}
